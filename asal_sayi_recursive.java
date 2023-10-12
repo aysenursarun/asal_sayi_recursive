dff516f205b7382af267ca6d676a00d09ad0dc2b
Ayşe Nur Sarun <117396227+aysenursarun@users.noreply.github.com>
2023-10-12 18:16:35 +0300

    asal_sayi_recursive

---
+++ asal_sayi_recursive.java
@@ -0,0 +1,33 @@
+package asal_sayi_recursive;
+
+import java.util.Scanner;
+
+public class asal_sayi_recursive {
+	
+	 public static void main(String[] args) {
+	        int num, prime ;
+	        Scanner scanner=new Scanner(System.in);
+	        System.out.print("Pozitif bir sayi giriniz : ");
+	        num= scanner.nextInt();
+
+	        prime=isPrime(num,num/2);
+
+	        if (prime==1){
+	            System.out.println(num+" sayisi asaldir");
+	        }else {
+	            System.out.println(num+" sayisi asal değildir");
+	        }
+	    }
+
+	    private static int isPrime(int num, int i) {
+	        if (i==1){
+	            return 1;
+	        }else {
+	            if (num%i==0){
+	                return 0;
+	            }
+	            else return isPrime(num,i-1);
+	        }
+	    }
+
+}
